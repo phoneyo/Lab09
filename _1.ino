d8e75450441a1f8671b390eb7247658ede580be3
phoneyo <asdzxc4563@gmail.com>
2017-06-05 19:54:29 +0800

    Add files via upload

---
+++ _1.ino
@@ -0,0 +1,29 @@
+// HCSR04Ultrasonic/examples/UltrasonicDemo/UltrasonicDemo.pde
+#include <Ultrasonic.h>
+#include <LiquidCrystal.h>
+#define TRIGGER_PIN 12
+#define ECHO_PIN 13
+LiquidCrystal lcd(9,8, 5, 4, 3, 2);
+Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
+
+void setup() 
+{
+Serial.begin(9600);
+lcd.begin(20, 2);
+}
+
+void loop()
+{
+
+float cmMsec, inMsec;
+long microsec = ultrasonic.timing();
+cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM); // 計算距離，單位: 公分
+inMsec = ultrasonic.convert(microsec, Ultrasonic::IN); // 計算距離，單位: 英吋
+Serial.print("MS: "); Serial.print(microsec);
+Serial.print(", CM: "); Serial.print(cmMsec);
+Serial.print(", IN: "); Serial.println(inMsec);
+lcd.print("CM:"); lcd.print(cmMsec);
+delay(1000);
+lcd.clear();
+
+}
